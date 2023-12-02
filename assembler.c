c8eca5c679253ab829f126f3d11b3472248ab89c
tr4v4 <nicola.travaglini3@studio.unibo.it>
2023-12-02 18:07:54 +0100

    clear-line

--- assembler.c
+++ assembler.c
@@ -1,10 +1,47 @@
 #include "assembler.h"
 
+const int MAX_LINE_LENGTH = 100;
+const int MAX_INSTRUCTION_LENGTH = 10;
+
+void clear_line(char line[], char instruction[], int *instruction_length) {
+    int line_index = 0;
+    int instruction_index = 0;
+
+    // Inserisco in instruction i caratteri di line eccetto SPACE e TAB
+    while (line[line_index] != '\0' &&
+           instruction_index < MAX_INSTRUCTION_LENGTH) {
+        if (line[line_index] != ' ' && line[line_index] != '\t' &&
+            line[line_index] != '\n') {
+            instruction[instruction_index] = line[line_index];
+            instruction_index++;
+        }
+        line_index++;
+    }
+
+    // Inserisco in instruction '\0'
+    instruction[instruction_index] = '\0';
+
+    *instruction_length = instruction_index;
+}
+
 void assemble(FILE *in) {
-    // test
+    // Scorro ogni riga
     while (!feof(in)) {
-        char str[100];
-        fgets(str, 100, in);
-        printf("%s", str);
+        char line[MAX_LINE_LENGTH];
+        fgets(line, MAX_LINE_LENGTH, in);
+
+        if (line[0] != '\n') {
+            char instruction[MAX_INSTRUCTION_LENGTH + 1];
+            int instruction_length = 0;
+
+            // Pulisco ogni riga non vuota
+            clear_line(line, instruction, &instruction_length);
+
+            // Identifico ogni istruzione (commento, A-instruction,
+            // C-instruction)
+            int type = identify_instruction(instruction);
+
+            printf("%s\t%d\n", instruction, instruction_length);
+        }
     }
 }
--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "assembler.h"
 
-
 int main(int argc, char *argv[]) {
     // Verifico il numero di argomenti (argc >= 2)
     if (argc < 2) {
@@ -13,6 +12,7 @@ int main(int argc, char *argv[]) {
     fin = fopen(argv[1], "r");
 
     // Assemblatore
+    // FIXME: deve prendere anche il nome del file
     assemble(fin);
 
     // Chiusura file di input
